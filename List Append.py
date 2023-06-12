7ccffeddc089fb648c79ea8e25b7ba04b63c1f95
KanchamK8 <136268495+KanchamK8@users.noreply.github.com>
2023-06-12 08:27:41 -0500

    Add files via upload

---
+++ List Append.py
@@ -0,0 +1,15 @@
+#List Append
+
+Puma = [1, "Cricket", 2, 3]
+Nike = [4, 5, "BasketBall"]
+
+Adidas = Puma + Nike
+print(Adidas)
+
+#List Extend
+
+Kent = [1, "Cricket", 2, 3]
+Mint = [4, 5, "BasketBall"]
+
+Current = Kent + Mint
+print(Current)
---
+++ Manager Employee.sql
@@ -0,0 +1,18 @@
+sp_helpdb
+
+select @@SERVERNAME
+
+SELECT * FROM dbo.spt_values;
+
+
+CREATE TABLE EMPLOYEES (employee_id int,employee_name varchar(255),manager_id int);
+
+INSERT INTO EMPLOYEES (employee_id, employee_name, manager_id) VALUES (1, 'Boss', 1), (3, 'Alice', 3), (2, 'Bob', 1), (4, 'Daniel', 2), (7, 'Luis', 4), (8, 'John', 3), (9, 'Angela', 8), (77, 'Robert', 1);
+
+SELECT * FROM EMPLOYEES;
+
+
+select employee_id as EMPLOYEE_ID from Employees where manager_id in
+(select employee_id from Employees WHERE manager_id in
+(select employee_id from Employees where manager_id =1))
+and employee_id !=1;
